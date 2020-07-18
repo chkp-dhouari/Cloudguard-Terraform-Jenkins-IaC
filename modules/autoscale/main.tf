2e678894dc98071f008f9258f13b299429f1039f
Dean Houari <dhouari@checkpoint.com>
2020-07-19 01:53:43 +0900

    Update main.tf

--- modules/autoscale/main.tf
+++ modules/autoscale/main.tf
@@ -29,7 +29,7 @@ resource "aws_launch_configuration" "asg_launch_configuration" {
   name_prefix = local.asg_name
   image_id = module.amis.ami_id
   instance_type = var.instance_type
-  key_name = var.key_name
+  # = var.key_name
   security_groups = [aws_security_group.permissive_sg.id]
   associate_public_ip_address = true
   iam_instance_profile = ( var.enable_cloudwatch ? aws_iam_instance_profile.instance_profile[0].name : "")
